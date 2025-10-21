ec4f2729bfac6d798948fe3fd2df9d0c7464bcc8
105700716 <105700716@student.swin.edu.au>
2025-10-21 21:38:42 +1100

    Update apply.php

--- apply.php
+++ apply.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Apply - Expression of Interest</title>
+</head>
+<body>
+  <h1>Expression of Interest Form</h1>
+  <p>Please fill out your details below and submit your application.</p>
+
+  <!-- The form starts here -->
+  <form action="process_eoi.php" method="post">
+    <label for="ref">Job Reference Number:</label><br>
+    <input type="text" name="ref" id="ref" required><br><br>
+
+    <label for="fname">First Name:</label><br>
+    <input type="text" name="fname" id="fname" required><br><br>
+
+    <label for="lname">Last Name:</label><br>
+    <input type="text" name="lname" id="lname" required><br><br>
+
+    <label for="email">Email Address:</label><br>
+    <input type="email" name="email" id="email" required><br><br>
+
+    <label for="phone">Phone Number:</label><br>
+    <input type="text" name="phone" id="phone" required><br><br>
+
+    <input type="submit" value="Submit Application">
+  </form>
+</body>
+</html>
 
